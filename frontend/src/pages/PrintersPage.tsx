b89dd993a32b62a454a7844d204a7b393f3a158a
Matteo Parenti <matteoparenti02@gmail.com>
2026-02-26 12:59:48 +0100

    Fix assignements for undefined

--- frontend/src/pages/PrintersPage.tsx
+++ frontend/src/pages/PrintersPage.tsx
@@ -2973,8 +2973,8 @@ function PrinterCard({
                                               trayId: slotIdx,
                                               trayInfo: {
                                                 type: filamentData.profile,
-                                                material: tray?.tray_type,
-                                                color: filamentData.colorHex || '',
+                                                material: tray?.tray_type ?? undefined,
+                                                color: filamentData.colorHex ?? undefined,
                                                 location: `${getAmsLabel(ams.id, ams.tray.length)} Slot ${slotIdx + 1}`,
                                               },
                                             }) : undefined,
@@ -3209,8 +3209,8 @@ function PrinterCard({
                                           trayId: htSlotId,
                                           trayInfo: {
                                             type: filamentData.profile,
-                                            material: tray?.tray_type,
-                                            color: filamentData.colorHex || '',
+                                            material: tray?.tray_type ?? undefined,
+                                            color: filamentData.colorHex ?? undefined,
                                             location: getAmsLabel(ams.id, ams.tray.length),
                                           },
                                         }) : undefined,
@@ -3404,8 +3404,8 @@ function PrinterCard({
                                             trayId: slotTrayId,
                                             trayInfo: {
                                               type: extFilamentData.profile,
-                                              material: extTray.tray_type,
-                                              color: extFilamentData.colorHex || '',
+                                              material: extTray.tray_type ?? undefined,
+                                              color: extFilamentData.colorHex ?? undefined,
                                               location: extLabel || t('printers.external'),
                                             },
                                           }),

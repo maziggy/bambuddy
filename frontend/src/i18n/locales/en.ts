32f4f1883701a796475cddc31bac235a83abe28e
Thomas Rambach <thomas.rambach@cadtoolbox.com>
2026-03-05 15:25:31 -0500

    Label updates for warnings and checkbox case

--- frontend/src/i18n/locales/en.ts
+++ frontend/src/i18n/locales/en.ts
@@ -3020,7 +3020,7 @@ export default {
     originalFilament: 'Original',
     overrideWith: 'Override with',
     resetToOriginal: 'Reset to original',
-    forceColorMatch: 'Force Color Match',
+    forceColorMatch: 'Force color match',
   },
 
   // Backup
